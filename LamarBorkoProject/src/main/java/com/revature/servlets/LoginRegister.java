6aaf8408bcfa973ade59690808a2823047bd4e65
Borko Stankovic <54279922+borkostankovic83@users.noreply.github.com>
2019-09-29 16:06:31 -0400

    Update LoginRegister.java

--- LamarBorkoProject/src/main/java/com/revature/servlets/LoginRegister.java
+++ LamarBorkoProject/src/main/java/com/revature/servlets/LoginRegister.java
@@ -3,13 +3,11 @@ package com.revature.servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Timestamp;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.model.Employee;
 import com.revature.repositories.EmployeeDao;
