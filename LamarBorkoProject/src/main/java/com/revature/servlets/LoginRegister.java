e34fb49262ae60e0ff2473adcde4084877e8ccab
Borko Stankovic <borkostankovic83@yahoo.com>
2019-09-30 09:03:22 -0400

    just make sure is update

--- LamarBorkoProject/src/main/java/com/revature/servlets/LoginRegister.java
+++ LamarBorkoProject/src/main/java/com/revature/servlets/LoginRegister.java
@@ -2,13 +2,11 @@ package com.revature.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Timestamp;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.model.Employee;
 import com.revature.repositories.EmployeeDao;
 import com.revature.repositories.EmployeeDaoJDBC;
@@ -30,16 +28,14 @@ public class LoginRegister extends HttpServlet {
 		String emp_username = req.getParameter("emp_username");
 		System.out.println(emp_username);
 		String emp_password = req.getParameter("emp_password");
-		String submitType = req.getParameter("submit");
-		
-		
-		String username = req.getParameter("emp_username");
-		String password = req.getParameter("emp_password");
 		String first_name = req.getParameter("first_name");
 		String last_name = req.getParameter("last_name");
 		String email = req.getParameter("email");
+		String username = req.getParameter("emp_username");
+		String password = req.getParameter("emp_password");
 		String emp_type = "Employee";
-		
+		String submitType = req.getParameter("submit");
+		System.out.println(first_name+last_name+email+username+password+emp_type);
 		
 		Employee employee = employeeDao.getEmployee(emp_username, emp_password);
 		if(submitType.equals("login") && (employee != null)) {
@@ -47,27 +43,24 @@ public class LoginRegister extends HttpServlet {
 			currentEmployee = employeeDao.getEmployee(emp_username);
 
 	    	if("Manager".equals(currentEmployee.emp_type)) {
-//	    		req.setAttribute("message", currentEmployee.first_name);
-//	    		System.out.println(employeeDao.getEmployee(currentEmployee.first_name));
 	    		req.getRequestDispatcher("manager.html").forward(req, resp);
 	    		PrintWriter out = resp.getWriter();
-	    		out.println(employeeDao.getEmployee(currentEmployee.first_name));
+	    		out.println("<html><body><section><h2>");
+	    		out.println("hi " + employeeDao.getEmployee(currentEmployee.first_name));
+	    		out.println("</h2></section></body></html>");
 	    	}else {
 	    		req.getRequestDispatcher("employee.html").forward(req, resp);
 	    	}					
 		}else if(submitType.equals("register")) {				
 				employeeDao.createEmployee(new Employee(id, first_name, last_name, email, username, password, emp_type));
-			
-			//employee.setFirst_name(req.getParameter(first_name));	
+				req.setAttribute("message", "thank you for registering log in");
+				req.getRequestDispatcher("registration_sucess.html").forward(req, resp);
+						
 		}else {
 			req.setAttribute("message", "Data not found, Create account!!");
 			req.getRequestDispatcher("new_user.html").forward(req, resp);
 		}
 	}
-
-
-	
-	
 	
 
 }
